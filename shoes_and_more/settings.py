78c384489526bd871d4cd597a6d5c81e9aa501c1
Dissy Ulina <dissyulina@gmail.com>
2022-01-07 17:25:18 +0000

    Complete hero image and its content

--- shoes_and_more/settings.py
+++ shoes_and_more/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
