a8006600b92f5817743c6fa93ffc27c449b5f66d
Dissy Ulina <dissyulina@gmail.com>
2022-01-13 15:25:59 +0000

    Add sorting by price and rating functionality

--- products/views.py
+++ products/views.py
@@ -3,6 +3,9 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category, Subcategory
 
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -11,19 +14,40 @@ def all_products(request):
     query = None
     categories = None
     subcategories = None
+    sort = None
+    direction = None
+    var_category = None
+    var_subcategory = None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
+            var_category = request.GET['category']
             categories = request.GET['category'].split(',')
+
             if 'subcategory' in request.GET:
+                var_subcategory = request.GET['subcategory']
                 subcategories = request.GET['subcategory'].split(',')
                 products = products.filter(category__name__in=categories, subcategory__name__in=subcategories)
                 subcategories = Subcategory.objects.filter(name__in=subcategories)
+
             else:
                 products = products.filter(category__name__in=categories)
 
             categories = Category.objects.filter(name__in=categories)
-
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -35,14 +59,19 @@ def all_products(request):
             queries = Q(name__icontains=query)
             products = products.filter(queries)
 
+    # current_sorting = f'{sort}_{direction}'
+  
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_subcategories': subcategories,
+        'var_category': var_category,
+        'var_subcategory': var_subcategory,
     }
 
     return render(request, 'products/products.html', context)
+
 
 
 def product_detail(request, product_id):
