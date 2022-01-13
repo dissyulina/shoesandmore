27775add3b499795821573ae8a0473cdfeb25cf9
Dissy Ulina <dissyulina@gmail.com>
2022-01-13 09:34:33 +0000

    Add category and subcategory filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category, Subcategory
 
 
 def all_products(request):
@@ -9,8 +9,22 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
+    subcategories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            if 'subcategory' in request.GET:
+                subcategories = request.GET['subcategory'].split(',')
+                products = products.filter(category__name__in=categories, subcategory__name__in=subcategories)
+                subcategories = Subcategory.objects.filter(name__in=subcategories)
+            else:
+                products = products.filter(category__name__in=categories)
+
+            categories = Category.objects.filter(name__in=categories)
+
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,6 +38,8 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_subcategories': subcategories,
     }
 
     return render(request, 'products/products.html', context)
