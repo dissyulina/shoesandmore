3a7b336cff2e3f36ef934c7a8d32bf4a385292ca
Dissy Ulina <dissyulina@gmail.com>
2022-01-17 15:35:01 +0000

    Add adjust bag and remove item from bag views, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
-    """ A view to render the shopping bag page """
-
-    return render(request, 'bag/bag.html')
+    """ A view that renders the bag contents page """
+    range_quantity = range(1, 21)
+    context = {
+        'range_quantity': range_quantity,
+    }
+    
+    return render(request, 'bag/bag.html', context)
 
 
 def add_to_bag(request, item_id):
@@ -32,4 +36,48 @@ def add_to_bag(request, item_id):
             bag[item_id] = quantity
 
     request.session['bag'] = bag
+
     return redirect(redirect_url)
+    
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        bag[item_id]['items_by_size'][size] = quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+
+    print ('range_quantity')
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
