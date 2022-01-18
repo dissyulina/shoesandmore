421f2d3b078740eea4af4aff23ae6ab866baa141
Dissy Ulina <dissyulina@gmail.com>
2022-01-18 17:27:11 +0000

    Add admin and signals to checkout app

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        import checkout.signals
