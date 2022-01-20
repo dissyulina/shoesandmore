1f36a221b09d6fcc9f0c1de5f5c06236bae1cfbc
Dissy Ulina <dissyulina@gmail.com>
2022-01-20 09:50:21 +0000

    Add template checkout

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout'),
+    path('', views.checkout, name='checkout')
 ]
--- shoes_and_more/settings.py
+++ shoes_and_more/settings.py
@@ -79,9 +79,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
-            'bulitins': [
+            'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
                 'crispy_forms.templatetags.crispy_forms_field',
             ]
