be3f7678a48d5db3a639069685de904e72e162f2
Dissy Ulina <dissyulina@gmail.com>
2022-01-24 11:41:55 +0000

    Create models and views for favorites, make migrations

--- favorites/models.py
+++ favorites/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+
+class Favorites(models.Model):
+    user = models.OneToOneField(
+        User, null=False, blank=False,
+        on_delete=models.CASCADE)
+    products = models.ManyToManyField(
+        Product, default="", through='FavoritesItem')
+
+    def __str__(self):
+        return f'Favorites ({self.user})'
+
+
+class FavoritesItem(models.Model):
+    favorites = models.ForeignKey(
+        Favorites, null=False, blank=False, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.name
--- favorites/views.py
+++ favorites/views.py
@@ -2,9 +2,44 @@ from django.shortcuts import render, redirect, reverse, HttpResponse, get_object
 from django.contrib import messages
 
 from products.models import Product
+# from profiles.models import UserProfile
+from favorites.models import Favorites, FavoritesItem
 
+# source: Python Django Ecommerce Customer Wishlist video https://www.youtube.com/watch?v=OgA0TTKAtqQ&t=138s 
 
 def view_favorites(request):
     """ A view that renders the favorites contents page """
 
-    return render(request, 'favorites/favorites.html')
+    favorites = None
+    # profile = get_object_or_404(UserProfile, user=request.user)
+    try:
+        favorites = Favorites.objects.get(user=profile)
+    except Favorites.DoesNotExist:
+        pass
+
+    context = {
+        'favorites': favorites,
+    }
+
+    return render(request, 'favorites/favorites.html', context=context)
+
+
+def add_to_favorites(request, item_id):
+    """ Add a specified product to the favorites """
+
+    product = get_object_or_404(Product, pk=item_id)
+
+    if request.method =='POST':
+        favorites, created = Favorites.objects.get_or_create(user=request.user)
+    
+        if WishlistItem.objects.filter(wishlist=wishlist, product=product).exists():
+            messages.error(request, f'{product.name} is removed from your favorites')
+            return redirect(reverse('product_detail', args=[product.id]))
+
+        else:
+            wishlist.products.add(product)
+            messages.success(request, f'{product.name} is added to your favorites')
+            return redirect(reverse('product_detail', args=[product.id]))
+            
+    return redirect(redirect_url)
+
