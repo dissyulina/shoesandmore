d59f2392e4cf3a5b37d8a999dc189863fa764894
Dissy Ulina <dissyulina@gmail.com>
2022-01-27 17:06:02 +0000

    Install stripe, handle card error on js, set keys in env variables

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -8,6 +8,11 @@
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
+// Pass the appearance object to the Elements instance
+const appearance = {
+    theme: 'stripe'
+  };
+
 var elements = stripe.elements();
 var style = {
     base: {
@@ -26,3 +31,22 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element'); 
+
+//const paymentElement = elements.create('payment');
+//paymentElement.mount('#payment-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
@@ -9,6 +13,10 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
