d92b8772625831689df6244c3530797b9fed5142
Dissy Ulina <dissyulina@gmail.com>
2022-01-28 10:18:49 +0000

    Add checkout post method and checkout success view

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('checkout_success/', views.checkout_success, name='checkout_success')
 ]
