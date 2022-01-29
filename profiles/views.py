68e26aed32418f7ecdacd412b1cfe950d784435e
Dissy Ulina <dissyulina@gmail.com>
2022-01-29 16:51:04 +0000

    Set up address form on profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
 
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
         'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
