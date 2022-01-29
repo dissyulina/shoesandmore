4a261eba3db46ff9cc01f4913383c48f4fcbed6f
Dissy Ulina <dissyulina@gmail.com>
2022-01-29 17:02:44 +0000

    Add script to country field on profile page and update success toast

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -9,6 +10,12 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
@@ -17,6 +24,7 @@ def profile(request):
         'profile': profile,
         'form': form,
         'orders': orders,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
