f5c1146280c86e5b109529313e0dd0403d89b72c
Dissy Ulina <dissyulina@gmail.com>
2022-01-29 11:25:19 +0000

    Complete functionality of webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,13 +12,15 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'street_address1',
               'street_address2', 'postcode', 'town_or_city',
               'country', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
