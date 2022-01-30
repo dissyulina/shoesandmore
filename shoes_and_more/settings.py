667be1c9aa73b8f141cc86c15c5763daecc3c9b8
Dissy Ulina <dissyulina@gmail.com>
2022-01-30 11:15:13 +0000

    Complete confirmation emails

--- shoes_and_more/settings.py
+++ shoes_and_more/settings.py
@@ -181,3 +181,4 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'shoesandmore@example.com'
