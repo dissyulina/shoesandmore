634373fa3ffb822b658767dd0e09fd53e08381c3
Dissy Ulina <dissyulina@gmail.com>
2022-02-02 17:27:02 +0000

    Create admin for favorites and reviews app

--- favorites/admin.py
+++ favorites/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
+from .models import Favorites, FavoritesItem
 
 
+class ProductInline(admin.StackedInline):
+    model = FavoritesItem
 
+
+class FavoritesAdmin(admin.ModelAdmin):
+    inlines = [ProductInline,]
+
+
+admin.site.register(Favorites, FavoritesAdmin)
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    """
+    Admin settings to display list of Reviews
+    """
+
+    model = Review
+    list_display = (
+        'product',
+        'date',
+        'user',
+        'rating',
+        'review_text',
+    )
+
+    ordering = ( '-product', '-date')
+    list_filter = ('rating',)
+
+
+admin.site.register(Review, ReviewAdmin)
