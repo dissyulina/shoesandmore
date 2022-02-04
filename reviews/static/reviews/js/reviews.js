e18b140aab2b5078a739dc2c8216adf92d5b2aee
Dissy Ulina <dissyulina@gmail.com>
2022-02-04 18:00:16 +0000

    Display rating on reviews into stars

--- reviews/static/reviews/js/reviews.js
+++ reviews/static/reviews/js/reviews.js
@@ -1,39 +1,3 @@
-/* Adapted from: https://codereview.stackexchange.com/questions/177945/convert-rating-value-to-visible-stars-using-fontawesome-icons/178069 */
-
-function getStars(stars) {
-    let output = [];
-    for (let i = stars; i >= 1; i--) {
-        output.push('<i class="fas fa-star"></i>');
-    }
-    for (let i = (5 - stars); i >= 1; i--) {
-        output.push('<i class="far fa-star"></i>');
-    }
-    return output.join('');
-}
-let starClass = $('.star-rating');
-console.log(getStars(4));
-
-$.fn.stars = function() {
-    return $(this).each(function() {
-        const numberOfStars = $(this).data("rating");
-        $(this).html = getStars(numberOfStars)
-    });
-}
-/*
-$(function(){
-    $('.stars').stars();
-});
-
-$.each($('.star-rating'), function() {
-    numberOfStars = $(this).data('rating');
-    $(this).html = getStars(numberOfStars)
-});
-
-$('.star-rating').each(function() {
-    numberOfStars = $(this).data('rating');
-    $(this).html = getStars(numberOfStars)
-})
-*/
 
 /* Prepopulate star rating on Edit Review Page */
 const editStars = $('#edit-rating').data("editstars");
