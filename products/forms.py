4600ba9f38c0fa48cbcee19b3c027b9a88db2175
Dissy Ulina <dissyulina@gmail.com>
2022-02-08 15:54:08 +0000

    Create product form for product admin

---
+++ products/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+    """ Product Form to add/ update products """
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        subcategories = Subcategory.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
