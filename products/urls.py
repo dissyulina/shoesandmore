a868f5ce9aab00ae130258ea074d4a20c9eac56b
Dissy Ulina <dissyulina@gmail.com>
2022-02-10 17:41:39 +0000

    Add functionality to edit product for product management

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('load-more/', views.load_more, name='load_more'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
