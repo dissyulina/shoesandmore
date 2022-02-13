f2aa511c7e76a1393ea8271edc01e77a96a506ea
Dissy Ulina <dissyulina@gmail.com>
2022-02-13 14:20:36 +0000

    Add carousel of best sellers products to the Homepage

--- home/views.py
+++ home/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 
 from blogs.models import Blog
+from products.models import Product
+from favorites.models import Favorites
 
 
 def index(request):
     """ Home Page """
-    
+
+    products = Product.objects.all().order_by('-total_purchased')[0:8]
     blogs = Blog.objects.all().order_by('-created_on')[0:3]
+
+    favorites = None
+    if request.user.is_authenticated:
+        try:
+            favorites = Favorites.objects.get(user=request.user)
+        except Favorites.DoesNotExist:
+            pass
+
     context = {
         'blogs': blogs,
+        'products': products,
+        'favorites': favorites,
     }
 
     return render(request, 'home/index.html', context)
