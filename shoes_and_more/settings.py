729d512ee55624ea17788f88cc9307da0347b432
Dissy Ulina <dissyulina@gmail.com>
2022-02-21 21:51:48 +0000

    Add cache control

--- shoes_and_more/settings.py
+++ shoes_and_more/settings.py
@@ -179,6 +179,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'shoes-and-more'
     AWS_S3_REGION_NAME = 'eu-central-1'
