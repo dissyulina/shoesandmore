0c8876cefae623d656f351ba7c35ab6ae3352d73
Dissy Ulina <dissyulina@gmail.com>
2022-02-22 13:22:29 +0000

    Fix typo on email configuration

--- shoes_and_more/settings.py
+++ shoes_and_more/settings.py
@@ -225,5 +225,5 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
