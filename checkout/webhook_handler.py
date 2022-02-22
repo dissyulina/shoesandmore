6b74ff9ab45711ed964b5700c8db998cad9c9231
Dissy Ulina <dissyulina@gmail.com>
2022-02-22 12:26:29 +0000

    Fix bug save order data to profile

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -66,6 +66,7 @@ class StripeWH_Handler:
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
+                profile.default_full_name = shipping_details.name
                 profile.default_phone_number = shipping_details.phone
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
